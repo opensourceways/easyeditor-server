2004a55eb619306dc15d00586d5f02f3eac8adcf
zhongjun2 <jun.zhongjun2@gmail.com>
2023-02-24 15:27:07 +0800

    Init easyedit server

---
+++ src/main/java/com/easyedit/easyedit/EasyeditApplication.java
@@ -0,0 +1,13 @@
+package com.easyedit.easyedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyeditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyeditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyedit/easyedit/EasyeditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyedit.easyedit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyeditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
