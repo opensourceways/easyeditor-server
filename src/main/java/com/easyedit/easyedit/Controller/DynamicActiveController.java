731bf8cbe2010b6517981b3f5347cecbf87d3f18
zhongjun2 <jun.zhongjun2@gmail.com>
2023-02-24 16:05:50 +0800

    Added apis for create/update/delete active page

---
+++ src/main/java/com/easyedit/easyedit/Controller/DynamicActiveController.java
@@ -0,0 +1,51 @@
+/* This project is licensed under the Mulan PSL v2.
+ You can use this software according to the terms and conditions of the Mulan PSL v2.
+ You may obtain a copy of Mulan PSL v2 at:
+     http://license.coscl.org.cn/MulanPSL2
+ THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ PURPOSE.
+ See the Mulan PSL v2 for more details.
+ Create: 2023
+*/
+
+package com.easyedit.easyedit.Controller;
+
+import java.io.IOException;
+
+import javax.naming.spi.ResolveResult;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dynamicactive")
+public class DynamicActiveController {
+    
+    @PostMapping(value = "")
+    public ResolveResult CreateDynamicActive(@RequestBody String items) throws IOException {
+        return null;
+    }
+
+    @GetMapping(value = "/page/{id}")
+    public ResolveResult GetDynamicActive(@PathVariable String id) throws IOException {
+        return null;
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResolveResult UpdateDynamicActive(@PathVariable String id, @RequestBody String items) {
+        return null;
+    }
+
+    @DeleteMapping(value = "/page/{id}")
+    public ResolveResult DeleteDynamicActive(@PathVariable String id) {
+        return null;
+    }
+}
+
--- src/main/java/com/easyedit/easyedit/EasyeditApplication.java
+++ src/main/java/com/easyedit/easyedit/EasyeditApplication.java
@@ -2,8 +2,10 @@ package com.easyedit.easyedit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.easyedit.*"})
 public class EasyeditApplication {
 
 	public static void main(String[] args) {
