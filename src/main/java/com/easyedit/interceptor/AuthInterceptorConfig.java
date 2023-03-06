94ed54a8ad349e36f983622248a6882885df3ab6
zhongjun2 <jun.zhongjun2@gmail.com>
2023-03-06 20:35:50 +0800

    Added api prefix in api

--- src/main/java/com/easyedit/interceptor/AuthInterceptorConfig.java
+++ src/main/java/com/easyedit/interceptor/AuthInterceptorConfig.java
@@ -11,23 +11,19 @@
 
 package com.easyedit.interceptor;
 
-import java.util.Arrays;
-import java.util.Optional;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class AuthInterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor())
-                .addPathPatterns("/page/**");
+                .addPathPatterns("/api/page/**");
     }
 
     @Bean
