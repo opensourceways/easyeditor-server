8bf74e6dfa13d50c072def3798d8ae898f96be07
zhongjun2 <jun.zhongjun2@gmail.com>
2023-03-09 16:06:02 +0800

    Added manage state api

---
+++ src/main/java/com/easyedit/controller/ManageStateController.java
@@ -0,0 +1,32 @@
+/* This project is licensed under the Mulan PSL v2.
+ You can use this software according to the terms and conditions of the Mulan PSL v2.
+ You may obtain a copy of Mulan PSL v2 at:
+     http://license.coscl.org.cn/MulanPSL2
+ THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ PURPOSE.
+ See the Mulan PSL v2 for more details.
+ Create: 2023
+*/
+
+package com.easyedit.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.easyedit.util.ResponseResult;
+
+@RestController
+@RequestMapping("/api/page")
+public class ManageStateController {
+
+    // @OneidToken
+    @PutMapping(value = "/{id}")
+    public ResponseResult SetPageState(@PathVariable String id, @RequestBody String state) {
+        return ResponseResult.errorResult(ResponseResult.AppHttpCodeEnum.ERROR);
+    }
+
+}
+
