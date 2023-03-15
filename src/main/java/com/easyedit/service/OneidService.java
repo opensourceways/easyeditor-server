9350d6e2436f2b02a3145f29728213c70bea9b70
zhongjun2 <jun.zhongjun2@gmail.com>
2023-03-15 18:08:32 +0800

    Added oneid api in service

--- src/main/java/com/easyedit/service/OneidService.java
+++ src/main/java/com/easyedit/service/OneidService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
@@ -29,9 +30,7 @@ public class OneidService {
     String g_community;
 
     public List<String> getUserPermission(String token, String cookie) {
-        String community = g_community;
-        String oneIdHost = g_authHost;
-        String url = String.format("%s/oneid/user/permissions?community=%s", oneIdHost, community);
+        String url = String.format("%s/oneid/user/permissions?community=%s", g_authHost, g_community);
 
         try {
             HttpResponse<JsonNode> response = Unirest.get(url)
@@ -44,6 +43,27 @@ public class OneidService {
             List<String> list = new ArrayList<>();
             for (int i = 0; i < jsonArray.length(); i++) {
                 list.add(jsonArray.getString(i));
+            }
+            return list;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<String> getUserSigPermission(String token, String cookie) {
+        String url = String.format("%s/query/user/ownertype?community=%s", g_authHost, g_community);
+
+        try {
+            HttpResponse<JsonNode> response = Unirest.get(url)
+                    .header("token", token)
+                    .header("Cookie", "_Y_G_=" + cookie)
+                    .asJson();
+            JSONArray jsonArray = response.getBody().getObject().getJSONArray("data");
+
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jSigRight = jsonArray.getJSONObject(i);
+                list.add(jSigRight.getString("sig"));
             }
             return list;
         } catch (Exception e) {
